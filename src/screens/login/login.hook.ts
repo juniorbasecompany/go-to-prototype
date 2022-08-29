945b241bf1beb113cd2bb4a10330da976856247d
soaresdiogo <diogoschsoares@gmail.com>
2022-08-29 23:31:03 +0100

    I added a login function

---
+++ src/screens/login/login.hook.ts
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import axios from "axios";
+import { BASE_URL } from '../../api/config';
+
+interface User {
+  username: string; 
+  password: string;
+}
+
+interface LoginProps  {
+  values: User;
+  onLogin: () => void;
+  onSetValues: (value: string, field: string) => void;
+}
+
+export const useLoginHelper = (): LoginProps => {
+  const initialValues = {username: '', password: ''};
+  const [values, setValues] = React.useState<User>(initialValues);
+
+  const handleLogin = () => {
+    axios.get(`${BASE_URL}/users`)
+    .then(response => {
+      const userLogged = response.data.find((user: User) => {
+        return user.username === values.username && user.password === values.password;
+      });
+      console.log(userLogged);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
+  const handleSetValues = (value: string, field: string) => {
+    setValues({...values, [field]: value});
+  }
+
+  return {
+    values,
+    onSetValues: handleSetValues,
+    onLogin: handleLogin,    
+  };
+};
--- src/screens/login/login.tsx
+++ src/screens/login/login.tsx
@@ -1,6 +1,10 @@
+import React from "react";
 import { Avatar, Box, Button, Container, Input } from "../../components";
+import { useLoginHelper } from "./login.hook";
 
 export const Login: React.FC = () => {
+  const { values, onSetValues, onLogin } = useLoginHelper();
+
   return (
     <Container align='center'>
       <Box border='solid'>
@@ -9,9 +13,21 @@ export const Login: React.FC = () => {
           size='lg'
           src='https://www.pngitem.com/pimgs/m/24-248235_user-profile-avatar-login-account-fa-user-circle.png'
         ></Avatar>
-        <Input size='lg' placeholder='Email' />
-        <Input size='lg' placeholder='Password' />
-        <Button size='lg' bg='primary'>
+        <Input
+          size='lg'
+          placeholder='Username'
+          name='username'
+          value={values.username}
+          onChange={(e) => onSetValues(e.target.value, "username")}
+        />
+        <Input
+          size='lg'
+          placeholder='Password'
+          name='password'
+          value={values.password}
+          onChange={(e) => onSetValues(e.target.value, "password")}
+        />
+        <Button size='lg' bg='primary' onClick={onLogin}>
           login
         </Button>
         <Button size='lg' bg='primary'>
