439087313f540d06981daae2da48f69112e796f4
soaresdiogo <diogoschsoares@gmail.com>
2022-08-30 21:25:59 +0100

    I added editor config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Detail from "./pages/detail/detail";
+import { Detail } from "./pages/detail/detail";
 import { Home } from "./pages/home/home";
 import { Login } from "./pages/login";
 
@@ -8,9 +8,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/detail' element={<Detail />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/detail" element={<Detail />} />
       </Routes>
     </Router>
   );
